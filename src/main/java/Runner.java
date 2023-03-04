1ec96f5f5a498e19602bd1e0ed5e177969b15fec
Nya Currie <nya1998@gmail.com>
2023-03-04 15:34:18 +0000

    static keyword

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,7 +3,7 @@ public class Runner {
     /**
      * This challenge is about interacting with static variables.
      *
-     * You task is simply to change the variable global_var from Class to the value of 'in' given in the parameters.
+     * Your task is simply to change the variable global_var from Class to the value of 'in' given in the parameters.
      * As you do so, notice that you're interacting with the CLASS and not an OBJECT. This is because due to the static
      * keyword. The static keyword causes methods and variables to belong to the class definition rather than an
      * object. We've seen Static used plenty of times: the main method is static because no objects could possibly
@@ -21,7 +21,7 @@ public class Runner {
      *
      * @param in the String value you should change global_var to in.
      */
-    public void changeClassVar(String in){
-
-    }
+        public void changeClassVar(String in) {
+            Class.global_var = in;
+        }
 }
